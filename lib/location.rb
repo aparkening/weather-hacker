9c93104606566e072467cc93f3f13ce2b16e91b3
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-06-08 23:00:03 +0000

    comment clean up

--- lib/location.rb
+++ lib/location.rb
@@ -30,8 +30,6 @@ class Location
   def weather
     #calls weather_scaper to get temperature and precipitation for self.
     w = WeatherScraper.new(self).import
-    #puts "The tempurature in #{w[0].name} is #{w[1]} and the chance of rain is #{w[2]}%. "
-   # w
   end  
 
 end  
