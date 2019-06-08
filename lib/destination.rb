2b246d8a8543da4e1c746d8ea6cef0eb57cab57c
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-06-08 21:17:11 +0000

    weather functionality

--- lib/destination.rb
+++ lib/destination.rb
@@ -1,7 +1,7 @@
 class Destination
   
   attr_accessor :name, :location, :weather, :temperature, :precipitation
-  
+
   def initialize(name, location)
     @name = name
     @location = location
@@ -11,14 +11,13 @@ class Destination
   def weather
     #calls weather_scaper to get temperature and precipitation for self.
     w = WeatherScraper.new(self).import
-    puts "The tempurature in #{w[0].name} is #{w[1]} and the chance of rain is #{w[2]}%."
+    #puts "The tempurature in #{@w[0].name} is #{@w[1]} and the chance of rain is #{@w[2]}%."
     w
   end  
   
   def weather_score 
-    #score = self.weather[1] - self.weather[2]
-    score = 5
-    puts "The weather score for #{self.name} is #{score}."
+    score = @w[1] - @w[2]
+    #puts "The weather score for #{self.name} is #{score}."
     #creates a weather index score using temperature and precipitation from #weather.
     
   end  
