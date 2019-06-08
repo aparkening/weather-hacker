88c97b32f6b41b984f238ce516616cb5e1905ece
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-06-08 15:32:58 +0000

    Location class outline

--- lib/location.rb
+++ lib/location.rb
@@ -1,6 +1,16 @@
 class Location 
   
+  attr_accessor :zipcode, :weather, :temperature, :precipitation
+  
+  def initialize(zipcode)
+    @zipcode = zipcode
+  end  
+  
   def list_destinations
-    #scrapes for list of zipcodes within 300 miles of self.
+    #scrapes www.freemaptools.com for list of zipcodes within 100 miles of self.
   end
+  
+  def weather 
+    #uses weather_scaper to determine temperature and precipitation for @zipcode.
+  end  
 end  
