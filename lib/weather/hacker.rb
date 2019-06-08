a622ab75ba52b34ff73c1f6c8aca4b653bbd46aa
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-06-08 01:27:47 +0000

    class files

--- lib/weather/hacker.rb
+++
@@ -1,8 +0,0 @@
-require "weather/hacker/version"
-
-module Weather
-  module Hacker
-    class Error < StandardError; end
-    # Your code goes here...
-  end
-end
--- lib/weather/hacker/version.rb
+++
@@ -1,5 +0,0 @@
-module Weather
-  module Hacker
-    VERSION = "0.1.0"
-  end
-end
