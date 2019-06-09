eec155f16e84422775a6f201e9c447b74e75791d
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-06-09 00:45:42 +0000

    menue functionality

--- lib/destination_finder.rb
+++ lib/destination_finder.rb
@@ -4,19 +4,27 @@ class DestinationFinder
   attr_accessor :location
   
   def call
-    puts "Welcome to Weather Hacker! Where are you today?"
+    puts "Welcome to Weather Hacker!
+Where are you today?"
+    puts ""
     puts "Enter your current city and state here:" 
-    input - gets.chomp
+    input = gets.chomp
     @location = Location.new(input)
      self.menu
   end
   
   def menu
-    @location.top_destinations
     #manage user navigation through the list.
-    #puts "What looks interesting to you?"
-    #links to Wikipedia page for response.
-    #puts "Enjoy your trip!"
+    @location.top_destinations
+    puts "What looks interesting to you?"
+    puts "Enter the number of a city on the list or exit to quit."
+    input = gets.chomp
+    case = input 
+    when == 1 
+      @location.dest_list[0]
+      #links to Wikipedia page for response.
+    when == exit
+      puts "Happy travelling!"
   end 
   
 end  
