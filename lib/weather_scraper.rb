59b6128080af353a3dd4f8e81e27059116c38bfe
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-06-14 01:44:41 +0000

    clean up

--- lib/weather_scraper.rb
+++ lib/weather_scraper.rb
@@ -9,10 +9,8 @@ class WeatherScraper
   def self.weather_location(location)
     mechanize = Mechanize.new
     page = mechanize.get("https://www.wunderground.com/")
-    #binding.pry
     form = page.forms.first 
     form['query'] = "#{location.name}"
-    #binding.pry
     page = form.submit
     page.uri
   end  
@@ -20,17 +18,12 @@ class WeatherScraper
   def self.scrape_weather(location)
     weather_array = []
     url = "#{self.weather_location(location)}"
-    #binding.pry
-    #url = "https://www.accuweather.com/en/us/grand-rapids-mi/49503/weather-forecast/329374"
     html = Nokogiri::HTML(open(url))
-    #binding.pry
     @temp = html.css("div.forecast-box-header .primary-temp .wu-value")[1].text.to_i
-    #binding.pry
     weather_array << @temp
     @precip_percent = html.css(".hook")[1].text[0..1].to_i
     weather_array << @precip_percent
     weather_array
-    #binding.pry
   end
 
 end  
