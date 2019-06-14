874ffa774a0ca0ef746e6966a913ea644b208999
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-06-14 02:20:05 +0000

    map_scraper

--- lib/location.rb
+++ lib/location.rb
@@ -14,7 +14,7 @@ class Location
   
   def top_destinations
     #scrapes www.freemaptools.com for list of destinations within 100 miles of self.
-    list = DestinationScraper.scrape_map
+    list = DestinationScraper.scrape_map(self)
     dest_list = list.collect{|city|Destination.new(city,self)}.sort_by{|dest|dest.weather_score}.reverse!
   end
   
