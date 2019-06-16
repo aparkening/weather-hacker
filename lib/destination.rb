573dc79dd31be09ab18d6b64cfa4682fd51845a0
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-06-16 20:14:26 +0000

    destination class cleanup

--- lib/destination.rb
+++ lib/destination.rb
@@ -2,16 +2,12 @@ require_relative './weather.rb'
 
 class Destination
   
-  attr_accessor :name, :location, :weather, :temp
+  attr_accessor :name, :location, :weather
 
   def initialize(place, location)
     @name = place
     self.location = location
     self.weather = Weather.new(self)
   end 
-  
-  def learn_more
-    WikiScraper.scrape(self)
-  end
 
 end  
