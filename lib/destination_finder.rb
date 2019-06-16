f5641a6a4442392da51fe6c00375da444739226f
JD <dirmeyer@gmail.com>
2019-06-16 16:03:59 -0400

    fix list_top_dest

--- lib/destination_finder.rb
+++ lib/destination_finder.rb
@@ -24,6 +24,7 @@ Where are you today?"
   
   def menu
     #manage user navigation through the list.
+    self.list_top_dest
     puts "Your top destinations are:" 
     @top_dest_list.each_with_index{|city, index| puts "#{index + 1}. #{city.name}: temperature-- #{city.weather.temp}, precipitation-- #{city.weather.precip}%."}
     puts ""
