cabac2a16addc4e9635ee721aacd7f727dba339b
aparkening <aaron.parkening@gmail.com>
2019-07-18 15:06:58 -0400

    Add begin rescue loop for weather_scraper.rb.

--- lib/weather_scraper.rb
+++ lib/weather_scraper.rb
@@ -19,27 +19,23 @@ class WeatherScraper
   def self.scrape_weather(place)
     weather_hash ={}
     url = "#{self.weather_url(place)}"
-    html = Nokogiri::HTML(open(url))
-    @temp_code = html.css("div.forecast-box-header .primary-temp .wu-value")
-    #binding.pry
-     i = 0
-    while @temp_code.text == ""
-    #binding.pry
+    
+    begin 
+      retries ||= 0
+      puts "I'm trying the #{retries} time"
       html = Nokogiri::HTML(open(url))
-      @temp_code = html.css("div.forecast-box-header .primary-temp .wu-value")
-      i = i + 1 
-      if i > 10
-        puts "We are experiencing difficulties. Please try again later."
-        puts ""
-        puts ""
-        puts ""
-        puts "-------------------------------------"
-        DestinationFinder.new.call
-      end  
-    end  
-      weather_hash[:temp] = @temp_code[1].text.to_i
+      @temp_code = html.css("div.forecast-box-header .primary-temp .u-value")
+      raise @temp_code.text.empty?
+    rescue TypeError
+      retries += 1
+      retry if retries < 10
+      puts "I've tried #{retries} times!"
+
+      DestinationFinder.new.call
+    end
+    weather_hash[:temp] = @temp_code[1].text.to_i
       
-   @precip_code = html.css(".hook")
+    @precip_code = html.css(".hook")
     while @precip_code.text == ""
     #binding.pry
       html = Nokogiri::HTML(open(url))
